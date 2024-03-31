028d1d916b1488227e67c82543e066a44b6e0bc6
ArielKandinov <165537762+ArielKandinov@users.noreply.github.com>
2024-03-31 00:04:59 -0400

    Create hack_simulation.bat

---
+++ hack_simulation.bat
@@ -0,0 +1,61 @@
+@echo off
+color 0a
+cls
+echo FLIPPER: Starting Hack...
+ping localhost -n 6 >nul
+echo SYSTEM32: Access secure files? Y/N
+ping localhost -n 3 >nul
+echo FLIPPER: Y
+echo .
+ping localhost -n 2 >nul
+echo .
+ping localhost -n 2 >nul
+echo .
+ping localhost -n 2 >nul
+echo .
+ping localhost -n 2 >nul
+echo .
+ping localhost -n 2 >nul
+echo .
+ping localhost -n 2 >nul
+echo .
+ping localhost -n 2 >nul
+echo .
+ping localhost -n 2 >nul
+echo .
+ping localhost -n 2 >nul
+echo SYSTEM32: You are Unauthorized. System Locked.
+ping localhost -n 6 >nul
+echo FLIPPER: Starting Brute-Force Hack
+ping localhost -n 3 >nul
+echo ..
+ping localhost -n 2 >nul
+echo ..
+ping localhost -n 2 >nul
+echo ..
+ping localhost -n 2 >nul
+echo ..
+ping localhost -n 2 >nul
+echo ..
+ping localhost -n 2 >nul
+echo ..
+ping localhost -n 2 >nul
+echo ..
+ping localhost -n 2 >nul
+echo ..
+ping localhost -n 2 >nul
+echo SYSTEM32: Authorized User Identified. Permission Granted
+ping localhost -n 3 >nul
+echo FLIPPER: Downloading Secure Files...
+ping localhost -n 3 >nul
+for /L %%x in (1, 1, 1000) do @echo 1010101010101010101010101010101010101010101010101010101010101010
+ping localhost -n 3 >nul
+echo FLIPPER: Hacking complete.
+ping localhost -n 3 >nul
+echo FLIPPER: Deleting Cached and Temp Files
+ping localhost -n 6 >nul
+echo FLIPPER: Done
+ping localhost -n 3 >nul
+echo FLIPPER: Exiting...
+ping localhost -n 3 >nul
+exit
